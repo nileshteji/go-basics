d16022930499b99f92de3c389e6c316d8d1ec38c
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-03-08 07:30:40 +0530

    Hello World

---
+++ HelloWorld.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Nilesh Teji")
+	nilesh()
+}
+
+func nilesh() {
+	fmt.Println("this is Function calling ")
+}
---
+++ paclage1.go
@@ -0,0 +1,11 @@
+package Programming
+
+import "fmt"
+
+func main() {
+	fmt.Println("Nilesh")
+}
+
+func nilesh() {
+	fmt.Println("hi My nilesh teji talking form the other package")
+}
