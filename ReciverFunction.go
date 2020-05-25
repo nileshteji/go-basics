064edee28b40a23c9d784675ad4fdd8ff87ba64c
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-05-25 22:12:12 +0530

    Version 3

---
+++ ReciverFunction.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+
+type deck []string
+
+func main(){
+
+cards := deck{"Nilesh","teji"}
+cards.print()
+
+// will give compile type error
+//
+// card := "nilesh"
+// card.print()
+
+}
+
+
+// this means any variable of type deck can access this method
+func (d deck) print()  {
+
+for i := 0 ; i< len(d) ; i++{
+    fmt.Println(d[i])
+  }
+
+}
