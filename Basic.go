e04fdc1105583cf9b224c997b341a2c4e90fed1a
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-05-25 21:58:21 +0530

    Version 1

---
+++ Basic.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+
+func main ()  {
+
+
+// this will give a compile time exception
+var nileshteji string=1+2;
+
+var nileshteji1 string ="nilesh"
+fmt.Println(nileshteji1)
+
+
+//Dynamic Declarition this includes the Declarition and Assignment
+ nilesh  := 1+2;
+fmt.Println(nilesh)
+
+
+
+}
+
+
+func nilesh121(card string){
+  fmt.Println(card)
+}
--- Type.go
+++ Type.go
@@ -9,10 +9,17 @@ type deck [] string
 
 func main()  {
   cards  := deck{"Nilesh","teji"}
+
+
+
+  
 // this works as standard for loop
   for index := 0;  index< len(cards); index++ {
   fmt.Println(cards[index])
   }
+
+
+
 //this works as a for  each loop in golang
   for _,index := range cards{
     fmt.Println(index)
