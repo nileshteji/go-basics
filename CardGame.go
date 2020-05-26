7632817bff41e5ac6f17f1c6f5851dba435559ae
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-05-26 12:03:26 +0530

    DONE

--- CardGame.go
+++ CardGame.go
@@ -1,47 +1,36 @@
 package main
-import ("fmt"
-  "strings"
+
+import (
+	"fmt"
+	"strings"
 )
+
 type deck []string
 
-func main(){
-
-cards := newDeck()
-fmt.Println(cards.toString())
-
-
-
-
-
-
-
-}
-func (d deck) toString() string {
-  return strings.Join(d," ")
+func main() {
+	cards := newDeck()
+	fmt.Println(cards.toByte())
 }
 
-
-func (d deck) hand(variable int) (deck,deck) {
-return d[:variable],d[variable:]
+func (d deck) toByte() []byte {
+	return []byte(strings.Join(d, " "))
+}
+func (d deck) hand(variable int) (deck, deck) {
+	return d[:variable], d[variable:]
 }
 
 func newDeck() deck {
 
+	cardSuites := deck{"Spades", "Hearts", "Diamonds", "Leaves"}
+	cardValues := deck{"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jacks", "Queen", "King"}
+	cards := deck{}
 
-cardSuites := deck{"Spades","Hearts","Diamonds","Leaves"}
-cardValues := deck{"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jacks","Queen","King"}
-cards := deck{}
+	for _, cardS := range cardSuites {
+		for _, cardV := range cardValues {
+			cards = append(cards, cardS+" of "+cardV)
+		}
+	}
 
-
-
-for _,cardS := range cardSuites{
- for _,cardV := range cardValues  {
-    cards=append(cards,cardS+" of "+ cardV)
-  }
-}
-
-
-
-return cards
+	return cards
 
 }
