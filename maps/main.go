a7289299e46b920209998f39ebc91ea851dadf32
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-05-28 17:52:42 +0530

    maps

---
+++ maps/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var color map[string]string
+	colors := make(map[string]string)
+
+	colors["nilesh"] = "teji"
+	colors["nilesh"] = "taheem"
+	colors["mrinank"] = "kumar"
+	colors["vineet"] = "kumar"
+	colors["savita"] = "taheem"
+
+	delete(colors, "nilesh")
+
+	for index, values := range colors {
+		fmt.Println(index + " " + values)
+	}
+	// values := map[string]string{
+	// 	"red": "#FFF",
+	// }
+
+}
